20b29f030469a28d2f9479ef6e7754d637462ce8
Fedexx2 <fedexx2@gmail.com>
2018-06-12 18:33:22 +0200

    build

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-export memo from './memo';
-export selector from './selector';
-export argsEquals from './argsEquals';
-export default from './rerex';
+import memo from './memo';
+import selector from './selector';
+import argsEquals from './argsEquals';
+import rerex from './rerex';
+
+rerex.memo = memo;
+rerex.selector = selector;
+rerex.argsEquals = argsEquals;
+
+export default rerex;
